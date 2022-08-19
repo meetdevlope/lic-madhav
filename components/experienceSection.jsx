c51ee68f2ebc5a773353fbef8c3b0f555612286e
meetdevlope <meetjpatel2412@gmail.com>
2022-08-19 10:56:53 +0530

    changes

--- components/experienceSection.jsx
+++ components/experienceSection.jsx
@@ -9,10 +9,15 @@ const ExperienceSection = () => {
         <div className="container flex justify-center flex-wrap gap-20 max-w-7xl mx-auto">
           <div className="flex-1 flex flex-col justify-center items-center tab:items-start">
             <h3>
-              <span className="text-brand">20+</span> Years of Experience
+              <span className="text-brand tab:text-5xl">20+</span> Years of Experience
             </h3>{" "}
-            <p className="mt-6 mb-12">
+            <p className="mt-14 mb-12 leading-6">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit,
+              dignissimos perspiciatis voluptatem nisi quibusdam iste, eius
+              consectetur deserunt adipisci harum nam amet odit omnis id! Totam
+              nesciunt cumque soluta quibusdam voluptas maiores, aliquid
+              necessitatibus accusantium nostrum unde, libero, perferendis
+              fugiat. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit,
               dignissimos perspiciatis voluptatem nisi quibusdam iste, eius
               consectetur deserunt adipisci harum nam amet odit omnis id! Totam
               nesciunt cumque soluta quibusdam voluptas maiores, aliquid
