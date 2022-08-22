8d899866def1e5380915269baeb0ed6afbfbba4b
meetdevlope <meetjpatel2412@gmail.com>
2022-08-22 16:07:07 +0530

    solutions and contact us added to the insurance page

--- pages/insurance.jsx
+++ pages/insurance.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 import ButtonComponent from '../components/buttonComponent'
+import ContactUsSection from '../components/contactUsSection'
 import SolutionCard from '../components/solutionCard'
+import Solutions from '../components/solutions'
 
 const Insurance = () => {
     return (
@@ -10,7 +12,7 @@ const Insurance = () => {
                 <div className="container flex justify-center flex-wrap gap-20 py-4 max-w-7xl mx-auto">
                     <div className="flex-1 flex flex-col justify-center tab:items-start items-center">
                         <h2>About Us</h2>
-                        <h4 className="mt-8 mb-4">
+                        <h4 className="mt-8 mb-4 text-lg">
                             Ajaykumar Virsangbhai Patel
                         </h4>
                         <p>AMFI Registered Mutual Fund Distributor empaneled with HDFC Mutual Fund.</p>
@@ -30,12 +32,15 @@ const Insurance = () => {
                 </div>
             </section>
 
+            <Solutions />
+
             <div className='flex flex-wrap gap-4' >
                 <SolutionCard />
                 <SolutionCard />
                 <SolutionCard />
                 <SolutionCard />
             </div>
+            <ContactUsSection />
         </>
     )
 }
